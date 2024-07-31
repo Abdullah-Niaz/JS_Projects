840c047eeb7da038ab10c004d9683ea7aaae32ce
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-01 01:05:00 +0500

    BMI Calculator

---
+++ 02_BMI_Calculator/script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector("#Height").value);
+    const weight = parseInt(document.querySelector("#Weight").value);
+    const result = document.querySelector("#result");
+
+    if (isNaN(height) || height <= 0) {
+        result.innerHTML = "Please provide a valid height.";
+    } else if (isNaN(weight) || weight <= 0) {
+        result.innerHTML = "Please provide a valid weight.";
+    } else {
+        result.innerHTML = (weight / ( ( height * weight) / 10000).toFixed(2));
+        // You can proceed with further processing here
+    }
+});
